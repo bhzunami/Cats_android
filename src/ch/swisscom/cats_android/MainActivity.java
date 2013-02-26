a1a0623f6575af05a25f63739d6d05630cb9da0c
TZHMANI7 <TZHMANI7@U233109.corproot.net>
2013-02-26 10:38:26 +0100

    Start of the correct navigation with a standard google login

--- src/ch/swisscom/cats_android/MainActivity.java
+++ src/ch/swisscom/cats_android/MainActivity.java
@@ -1,24 +1,27 @@
 package ch.swisscom.cats_android;
 
-import org.json.JSONException;
+import java.util.ArrayList;
 
-import android.os.Bundle;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class MainActivity extends Activity {
 
 	public static final String PREFS_NAME = "UserAccount";
 	
 	private String user = null;
-//	private String password = null;
-	// InputsFields
-	EditText userName;
-	EditText userPassword;
 	
 	SharedPreferences settings = null;
 
@@ -27,29 +30,23 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// Get the inputFileds and SharedPreferences
+		// Get SharedPreferences
 		this.initalize();
 
-		// get the shared preference
+		// get the userName to check if user is logged in
 		this.user = settings.getString("user", "");
-		// check if the user is still logged in.
-		if( this.user != "")
-			try {
-				this.getJson( this.findViewById( R.id.btn_get_json) );
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		if( this.user == "") {
+			this.goToLoginView();
+		}
 		
-//		if (this.userName != null)
-//			this.userName.setText(this.user);
+		// Get the Json from Server
+		this.executeHttpRequest();
 		
-//		this.password = settings.getString("password", "");
-//		
-//		if(this.userPassword != null)
-//			this.userPassword.setText(this.password);
-
+		//TODO: parse Json array in a ArrayList<String>
+		// initalize the list view
+		this.createListView( this.getDataFromJson() );
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,27 +55,102 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void getJson(View view) throws JSONException {
-
-		// Set the input
-		SharedPreferences.Editor editor = this.settings.edit();
-		editor.putString("user", this.userName.getText().toString());
-		editor.putString("password", this.userPassword.getText().toString());
-
-		// commit the shit
-		editor.commit();
-		
-		// Start the new acitivity
-		Intent intent = new Intent(this, GetJsonActivity.class);
-		startActivity(intent);
-	}
 	
 	private void initalize() {
-		// Get the shared preferenes
+		// Get the sharedPreferenes
 		this.settings = getSharedPreferences(PREFS_NAME, 0);
-		// get the editTexts
-		this.userName = (EditText) findViewById(R.id.etxt_user_name);
-		this.userPassword = (EditText) findViewById(R.id.etxt_password);
+	}
+	
+	
+	public void executeHttpRequest() {
+		// perform request to server
+		HttpHandler.get("time", null, new JsonHttpResponseHandler() {
+			
+			public void onSuccess(JSONArray array) {
+				// Pull out the first event on the public timeline
+				System.out.println("Success" +array);
+				
+				JSONObject firstEvent = null;
+				
+				try {
+					firstEvent = (JSONObject) array.get(0);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				String text = null;
+				try {
+					text = firstEvent.getString("res");
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+//
+				// Do something with the response
+				System.out.println(text);
+			}
+
+			@Override
+			public void onFailure(Throwable e, String response) {
+				// Response failed :(
+				System.out.println("Fehler");
+			}
+
+			@Override
+			public void onFinish() {
+				// Response failed :(
+				System.out.println("Fertig");
+			}
+		}
+
+		);}
+	
+	
+	// Parse the Json into ArrayList<String>
+	private ArrayList<String> getDataFromJson() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.add("Android");
+		values.add("1");
+		values.add("2");
+		values.add("3");
+		values.add("4");
+		values.add("5");
+		values.add("6");
+		values.add("6");
+		values.add("7");
+		return values;
+		
+	}
+	
+	
+	private void createListView(ArrayList<String> list) {
+		ArrayAdapter<String> adapter;
+		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list );
+		
+		ListView listView = (ListView) findViewById(R.id.list_jsonData);
+		
+		listView.setAdapter( adapter );
+	}
+	
+	public void logoutUser(View view) {
+		if(settings == null)
+			return;
+
+			SharedPreferences.Editor editor = this.settings.edit();
+			editor.putString("user", null);
+			editor.putString("password", null);
+
+			editor.commit();
+			
+			this.goToLoginView();
+			
+	}
+	
+	private void goToLoginView() {
+		Intent intent = new Intent(this,LoginActivity.class);
+		startActivity(intent);
 	}
 	
 }
